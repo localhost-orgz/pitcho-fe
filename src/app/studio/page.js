0066367bf5e257088e74e6715306297c59cb9fce
fazamumtaz <fazamumtazramadhan@gmail.com>
2026-05-25 09:06:29 +0700

    feat: 2nd row studio

--- src/app/studio/page.js
+++ src/app/studio/page.js
@@ -201,7 +201,7 @@ export default function StationPage() {
         </div>
 
         <div className="w-full col-span-1 flex flex-col gap-3">
-          <div className="w-full bg-orange-400/10 gap-3 rounded-2xl border-bold p-6 flex items-center">
+          <div className="w-full bg-linear-to-l from-white via-white to-orange-500/5 gap-3 rounded-2xl border-bold p-6 flex items-center">
             <Image
               src={"/streak.svg"}
               width={100}
@@ -243,89 +243,83 @@ export default function StationPage() {
         </div>
       </div>
       <div className="grid grid-cols-3 gap-6">
-        <div className="w-full col-span-2 p-6 bg-white rounded-2xl border-bold">
-          <div className="flex flex-col">
-            <h2 className="text-2xl font-bold">Today's plan</h2>
-            <p className="text-slate-500 mt-1">Let's get better togather!</p>
+        <div className="w-full col-span-2 px-5 py-4 bg-white rounded-2xl border-bold">
+          <div className="flex justify-between">
+            <h2 className="text-xl font-bold">Continue Your Practice</h2>
+            <div className="flex items-center text-main text-sm font-semibold gap-1">
+              <span>View all</span>
+              <ChevronRight size={18} />
+            </div>
           </div>
 
-          <div className="grid grid-cols-3 gap-5 mt-7">
-            <div className="w-full bg-white border-2 rounded-2xl p-5 gap-7 col-span-1 flex flex-col">
-              <div className="flex flex-col">
-                <span className="text-sm font-bold">Today's Goal</span>
-                <span className="text-sm text-slate-500">
-                  Practice for 15 minutes
+          <div className="grid grid-cols-2 gap-5 mt-7">
+            <div className="w-full bg-blue-500/10 border-2 rounded-2xl p-5 gap-7 col-span-1 flex flex-col">
+              <div className="flex flex-col gap-1">
+                <span className="text-xl font-bold text-blue-500">
+                  Presentation Mode
+                </span>
+                <span className="text-sm text-slate-500 w-[60%]">
+                  Practice your public speaking with virtual audiences and
+                  distractions.
                 </span>
               </div>
-              <div className="flex flex-col gap-2">
-                <div className="flex items-end gap-0.5">
-                  <span className="text-lg font-bold">0 / 15</span>
-                  <span className="font-medium text-sm mb-0.5 text-slate-500">
-                    min
-                  </span>
-                </div>
-                <div className="w-full bg-border h-1 rounded-2xl"></div>
-              </div>
+              <Button className="flex flex-row gap-2 text-blue-500 bg-white w-fit items-center py-2 px-5 rounded-2xl font-bold">
+                <span>Continue</span>
+                <ArrowRight size={18} strokeWidth={2.5} />
+              </Button>
             </div>
-
-            <div className="w-full bg-linear-to-br from-white via-white to-sky-200/20 border-bold relative p-5 col-span-2 flex flex-col justify-between ">
-              <div className="flex flex-col">
-                <span className="text-sm font-bold text-slate-800">
-                  Suggested for you
+            <div className="w-full bg-emerald-600/10 border-2 rounded-2xl p-5 gap-7 col-span-1 flex flex-col">
+              <div className="flex flex-col gap-1">
+                <span className="text-xl font-bold text-emerald-500">
+                  Interview Mode
                 </span>
-                <span className="text-xs font-bold text-slate-400 mt-1 leading-normal">
-                  Focus on maintaining eye contact and reducing filler words.
+                <span className="text-sm text-slate-500 w-[60%]">
+                  Prepeare for you dream job with AI Mock intervies based on
+                  your CV.
                 </span>
               </div>
-              <Link
-                className="text-xs font-extrabold flex items-center text-main group-hover:text-sky-600 transition-colors mt-4"
-                href={"/"}
-              >
-                See tips{" "}
-                <ArrowRight className="w-4 h-4 ml-1 transition-transform group-hover:translate-x-1" />
-              </Link>
+              <Button className="flex flex-row gap-2 bg-white text-emerald-600 w-fit items-center py-2 px-5 rounded-2xl font-bold">
+                <span>Continue</span>
+                <ArrowRight size={18} strokeWidth={2.5} />
+              </Button>
             </div>
           </div>
         </div>
 
-        <div className="w-full col-span-1 flex flex-col gap-3">
-          <div className="w-full bg-orange-400/10 gap-3 rounded-2xl border-bold p-6 flex items-center">
-            <Image
-              src={"/streak.svg"}
-              width={100}
-              height={100}
-              className="w-9 h-9 mb-1"
-              alt="streak"
-            />
-            <div className="flex flex-col">
-              <span className="font-bold">12 Days Streak</span>
-              <span className="text-sm text-slate-500">keep it going!</span>
+        <div className="w-full px-5 py-4 col-span-1 flex flex-col gap-3 bg-white border-bold">
+          <div className="flex items-center justify-between">
+            <h2 className="text-xl font-bold">Recent Sessions</h2>
+            <div className="flex items-center text-main text-sm font-semibold gap-1">
+              <span>View all</span>
+              <ChevronRight size={18} />
             </div>
           </div>
-          <div className="w-full bg-indigo-400/10 gap-3 rounded-2xl border-bold p-6 flex items-center">
-            <Image
-              src={"/target.svg"}
-              width={100}
-              height={100}
-              className="w-9 h-9 mb-1"
-              alt="dart"
-            />
+          {/* recent detail card */}
+          <div className="w-full border-2 rounded-xl py-3 px-4 flex flex-col justify-between h-full">
             <div className="flex flex-col">
-              <span className="font-bold">12 Days Streak</span>
-              <span className="text-sm text-slate-500">keep it going!</span>
+              <h6 className="font-bold">Final Thesis Presentation</h6>
+              <div className="flex items-center text-slate-500 font-bold text-xs gap-1">
+                <span className="">May 2026, 17</span>
+                <span>·</span>
+                <span>10:30 AM</span>
+              </div>
             </div>
-          </div>
-          <div className="w-full bg-yellow-400/10 gap-3 rounded-2xl border-bold p-6 flex items-center">
-            <Image
-              src={"/trophy.svg"}
-              width={100}
-              height={100}
-              className="w-9 h-9 mb-1"
-              alt="trophy"
-            />
-            <div className="flex flex-col">
-              <span className="font-bold">12 Days Streak</span>
-              <span className="text-sm text-slate-500">keep it going!</span>
+
+            <div className="flex flex-col gap-3 mb-2">
+              <h6 className="font-bold">Performance Overview</h6>
+              <div className="flex gap-4">
+                <div className="h-20 w-20 rounded-full bg-slate-500 shrink-0"></div>
+                <div className="flex flex-col">
+                  <span className="font-bold">Good Job!</span>
+                  <span className="text-slate-500 text-xs w-[70%]">
+                    You showed improvement in focus and clarity.
+                  </span>
+                  <div className="flex items-center text-main font-bold text-xs mt-2">
+                    See full report
+                    <ChevronRight size={15} />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
